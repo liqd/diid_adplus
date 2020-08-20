b79ecdd4903d494cb2d6e3add0b0ba082192309f
Robert Mader <r.mader@liqd.net>
2020-08-20 18:33:29 +0200

    apps/users: Add a saml2 specific helper func
    It allows to set a username if we only got an email from the saml2
    IDP.

--- apps/users/models.py
+++ apps/users/models.py
@@ -160,3 +160,8 @@ class User(auth_models.AbstractBaseUser, auth_models.PermissionsMixin):
             organisation=organisation,
             has_agreed=True
         ).exists()
+
+    def set_username_from_email(self, email):
+        if not self.username:
+            name = email[0].split("@", 1)[0]
+            self.username = name
