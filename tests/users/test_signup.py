189ae2b72a99b43882fcd337910cae670f599e5c
Julian Dehm <j.dehm@liqd.net>
2022-04-28 15:10:07 +0200

    tests: fix and reenable the user tests

--- tests/users/test_signup.py
+++ tests/users/test_signup.py
@@ -9,17 +9,19 @@ from apps.users.models import User
 @pytest.mark.django_db
 def test_signup_user_newsletter_checked(client):
     resp = client.post(
-        reverse('account_signup'), {
-            'username': 'dauser',
-            'email': 'mail@example.com',
-            'get_newsletters': 'on',
-            'password1': 'password',
-            'password2': 'password',
-            'terms_of_use': 'on',
-            'captcha': 'testpass:0',
-            'data_protection': 'on',
-            'get_notifications': 'on',
-        })
+        reverse("account_signup"),
+        {
+            "username": "dauser",
+            "email": "mail@example.com",
+            "get_newsletters": "on",
+            "password1": "password",
+            "password2": "password",
+            "terms_of_use": "on",
+            "captcha": "testpass:0",
+            "data_protection": "on",
+            "get_notifications": "on",
+        },
+    )
     assert resp.status_code == 302
     user = User.objects.get()
     assert user.get_newsletters
@@ -28,16 +30,18 @@ def test_signup_user_newsletter_checked(client):
 @pytest.mark.django_db
 def test_signup_user_newsletter_not_checked(client):
     resp = client.post(
-        reverse('account_signup'), {
-            'username': 'dauser',
-            'email': 'mail@example.com',
-            'password1': 'password',
-            'password2': 'password',
-            'terms_of_use': 'on',
-            'captcha': 'testpass:0',
-            'data_protection': 'on',
-            'get_notifications': 'on',
-        })
+        reverse("account_signup"),
+        {
+            "username": "dauser",
+            "email": "mail@example.com",
+            "password1": "password",
+            "password2": "password",
+            "terms_of_use": "on",
+            "captcha": "testpass:0",
+            "data_protection": "on",
+            "get_notifications": "on",
+        },
+    )
     assert resp.status_code == 302
     user = User.objects.get()
     assert not user.get_newsletters
@@ -46,15 +50,17 @@ def test_signup_user_newsletter_not_checked(client):
 @pytest.mark.django_db
 def test_signup_user_unchecked_terms_of_use(client):
     resp = client.post(
-        reverse('account_signup'), {
-            'username': 'dauser',
-            'email': 'mail@example.com',
-            'password1': 'password',
-            'password2': 'password',
-            'captcha': 'testpass:0',
-            'data_protection': 'on',
-            'get_notifications': 'on',
-        })
+        reverse("account_signup"),
+        {
+            "username": "dauser",
+            "email": "mail@example.com",
+            "password1": "password",
+            "password2": "password",
+            "captcha": "testpass:0",
+            "data_protection": "on",
+            "get_notifications": "on",
+        },
+    )
     assert User.objects.count() == 0
     assert not resp.context["form"].is_valid()
     assert list(resp.context["form"].errors.keys()) == ["terms_of_use"]
@@ -73,6 +79,8 @@ def test_signup_user_without_captcha(client):
             "password1": "password",
             "password2": "password",
             "terms_of_use": "on",
+            "data_protection": "on",
+            "get_notifications": "on",
         },
     )
     assert resp.status_code == 302
@@ -93,6 +101,8 @@ def test_signup_user_when_not_captcha(client):
             "password1": "password",
             "password2": "password",
             "terms_of_use": "on",
+            "data_protection": "on",
+            "get_notifications": "on",
         },
     )
     assert resp.status_code == 302
