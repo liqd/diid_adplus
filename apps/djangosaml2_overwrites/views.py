bafd3f18b9ee04d17a73b1a7dd7205a9a4589e69
Julian Dehm <j.dehm@liqd.net>
2024-06-06 11:26:37 +0200

    apps/djangosaml2_overwrites: fix deprecation of ugettext

--- apps/djangosaml2_overwrites/views.py
+++ apps/djangosaml2_overwrites/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.sites.shortcuts import get_current_site
 from django.shortcuts import get_object_or_404
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from django.views import generic
 from djangosaml2.utils import validate_referral_url
 
@@ -11,14 +11,14 @@ from apps.users.models import User
 from . import forms
 
 
-class Djangosaml2SignupView(LoginRequiredMixin,
-                            SuccessMessageMixin,
-                            generic.UpdateView):
+class Djangosaml2SignupView(
+    LoginRequiredMixin, SuccessMessageMixin, generic.UpdateView
+):
 
     model = User
-    template_name = 'djangosaml2_overwrites/signup.html'
+    template_name = "djangosaml2_overwrites/signup.html"
     form_class = forms.Djangosaml2SignupForm
-    success_message = _('Your profile was successfully updated.')
+    success_message = _("Your profile was successfully updated.")
 
     def get_object(self):
         return get_object_or_404(User, pk=self.request.user.id)
@@ -33,7 +33,11 @@ class Djangosaml2SignupView(LoginRequiredMixin,
         ret = super().get_context_data(**kwargs)
         redirect_field_value = self.request.GET.get("next")
         site = get_current_site(self.request)
-        ret.update({"site": site,
-                    "redirect_field_name": "next",
-                    "redirect_field_value": redirect_field_value})
+        ret.update(
+            {
+                "site": site,
+                "redirect_field_name": "next",
+                "redirect_field_value": redirect_field_value,
+            }
+        )
         return ret
