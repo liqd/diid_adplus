b5feaafe37bb57b8cea212998fc8c8994770603f
Julian Dehm <j.dehm@liqd.net>
2024-10-21 17:41:21 +0200

    fix saml login errors after rebase on a+

--- apps/djangosaml2_overwrites/overwrites.py
+++ apps/djangosaml2_overwrites/overwrites.py
@@ -3,8 +3,6 @@ from django.urls import reverse
 from djangosaml2.urls import urlpatterns
 from djangosaml2.views import AssertionConsumerServiceView
 
-from apps.users.models import User
-
 from .urls import urlpatterns as custom_urlpatterns
 
 
@@ -15,21 +13,19 @@ def apply_custom_overwrites():
 
 
 def customize_session(self, user, session_info):
-    user_obj = User.objects.get(email=user)
-    if not EmailAddress.objects.filter(user=user_obj, email=user).exists():
-        email_address = EmailAddress(user=user_obj, email=user)
+    if not EmailAddress.objects.filter(user=user, email=user.email).exists():
+        email_address = EmailAddress(user=user, email=user.email)
         email_address.save()
-    if not EmailAddress.objects.filter(user=user_obj, primary=True).exists():
-        email_address = EmailAddress.objects.get(user=user_obj, email=user)
+    if not EmailAddress.objects.filter(user=user, primary=True).exists():
+        email_address = EmailAddress.objects.get(user=user, email=user.email)
         email_address.primary = True
         email_address.save()
 
 
 def custom_redirect(self, user, relay_state, session_info):
-    user_obj = User.objects.get(email=user)
-    email_address = EmailAddress.objects.get(user=user_obj, email=user)
+    email_address = EmailAddress.objects.get(user=user, email=user.email)
     if not email_address.verified:
-        signup = reverse('saml2_signup')
+        signup = reverse("saml2_signup")
         if relay_state:
             signup += "?next={}".format(relay_state)
         return signup
